e6790f64d27f8c7f2096573e8110134cc59182ee
Oleg V. Zhitnik <blacklion.ozv@gmail.com>
2012-06-12 18:31:41 +0300

    Creat project

---
+++ PadCMS-CocoaTouch-Core/PadCMS_CocoaTouch_Core.h
@@ -0,0 +1,13 @@
+//
+//  PadCMS_CocoaTouch_Core.h
+//  PadCMS-CocoaTouch-Core
+//
+//  Created by Oleg Zhitnik on 12.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PadCMS_CocoaTouch_Core : NSObject
+
+@end
---
+++ PadCMS-CocoaTouch-Core/PadCMS_CocoaTouch_Core.m
@@ -0,0 +1,13 @@
+//
+//  PadCMS_CocoaTouch_Core.m
+//  PadCMS-CocoaTouch-Core
+//
+//  Created by Oleg Zhitnik on 12.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PadCMS_CocoaTouch_Core.h"
+
+@implementation PadCMS_CocoaTouch_Core
+
+@end
