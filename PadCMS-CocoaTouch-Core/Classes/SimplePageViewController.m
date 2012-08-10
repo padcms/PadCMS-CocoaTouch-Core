806cdb2db85618d593b33a6e6537e2b4c6a1ffa5
getmanenko_igor <igor.getmanenko89@gmail.com>
2012-08-10 14:26:47 +0300

    video in miniarticles fixed

--- PadCMS-CocoaTouch-Core/Classes/SimplePageViewController.m
+++ PadCMS-CocoaTouch-Core/Classes/SimplePageViewController.m
@@ -43,7 +43,10 @@
 	if (!_page.isComplete) [self showHUD];
 	if (!_page.isComplete) return;
     
-  
+    tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction:)];
+	tapGestureRecognizer.cancelsTouchesInView = NO;
+	tapGestureRecognizer.delegate = self;
+    [self.view  addGestureRecognizer:tapGestureRecognizer];
     
 	[self loadBackground];	
 
@@ -62,10 +65,6 @@
 {
     [super viewDidAppear:animated];
     [self createVideoFrame];
-    tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction:)];
-	tapGestureRecognizer.cancelsTouchesInView = NO;
-	tapGestureRecognizer.delegate = self;
-    [self.view  addGestureRecognizer:tapGestureRecognizer];
 }
 
 -(void)loadBackground
@@ -166,6 +165,7 @@
         }
         else
         {
+            NSLog(@"url - %@", activeZone.URL);
             PCPageElementVideo *video = [[PCPageElementVideo alloc] init];
             video.stream = activeZone.URL;
             CGRect videoRect = activeZone.rect;
