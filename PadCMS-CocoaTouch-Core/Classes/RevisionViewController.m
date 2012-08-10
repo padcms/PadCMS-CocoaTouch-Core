2547a74cb05edf553bd45e820b0184f40f404674
Maxim Pervushin <exxenos@gmail.com>
2012-08-10 16:06:29 +0400

    Summary view implemented.

--- PadCMS-CocoaTouch-Core/Classes/RevisionViewController.m
+++ PadCMS-CocoaTouch-Core/Classes/RevisionViewController.m
@@ -16,6 +16,7 @@
 #import "PCPageViewController.h"
 #import "PCResourceCache.h"
 #import "PCScrollView.h"
+#import "PCSummaryView.h"
 #import "PCTocView.h"
 #import "PCVideoManager.h"
 
@@ -451,10 +452,46 @@
         } else {
             return CGSizeMake(250, 192 /*viewSize.height / 4*/);
         }
+    } else if (tocView == hudView.summaryView.gridView) {
+        return CGSizeMake(314, 100);
     }
     
     return CGSizeZero;
 }
+
+- (UIImage *)hudView:(PCHudView *)hudView summaryImageForIndex:(NSUInteger)index
+{
+    NSArray *tocItems = _revision.validVerticalTocItems;
+    
+    if (tocItems != nil && tocItems.count > index) {
+        PCTocItem *tocItem = [tocItems objectAtIndex:index];
+        
+        PCResourceCache *cache = [PCResourceCache defaultResourceCache];
+        NSString *imagePath = [_revision.contentDirectory stringByAppendingPathComponent:tocItem.thumbSummary];
+        UIImage *image = [cache objectForKey:imagePath];
+        if (image == nil) {
+            image = [UIImage imageWithContentsOfFile:imagePath];
+            [cache setObject:image forKey:imagePath];
+        }
+        
+        return image;
+    }
+    
+    return nil;
+}
+
+- (NSString *)hudView:(PCHudView *)hudView summaryTextForIndex:(NSUInteger)index
+{
+    NSArray *tocItems = _revision.validVerticalTocItems;
+    
+    if (tocItems != nil && tocItems.count > index) {
+        PCTocItem *tocItem = [tocItems objectAtIndex:index];
+        return tocItem.title;
+    }
+    
+    return nil;
+}
+
 
 - (UIImage *)hudView:(PCHudView *)hudView tocImageForIndex:(NSUInteger)index
 {
@@ -514,11 +551,8 @@
             return;
         }
         
-//        [horizontalScrollView scrollRectToVisible:CGRectMake(1024 * index, 0, 1024, 768) animated:YES];
-        
     } else {
         PCTocItem *tocItem = [_revision.validVerticalTocItems objectAtIndex:index];
-//        NSInteger pageIndex = -1;
         NSArray *revisionPages = _revision.pages;
         for (PCPage *page in revisionPages) {
             if (page.identifier == tocItem.firstPageIdentifier) {
@@ -526,8 +560,6 @@
                 break;
             }
         }
-        
-//        [self showPageWithIndex:pageIndex];
     }
 }
 
@@ -536,11 +568,14 @@
     if (tocView == hudView.topTocView) {
         [hudView.topBarView hideKeyboard];
     }
+    
+    if (tocView == hudView.bottomTocView && state == PCTocViewStateHidden) {
+        [_hudView hideSummaryAnimated:YES];
+    }
 }
 
 - (void)hudView:(PCHudView *)hudView didTransitToc:(PCTocView *)tocView toState:(PCTocViewState)state
 {
-    
 }
 
 #pragma makr - PCTopBarViewDelegate
@@ -553,6 +588,11 @@
 
 - (void)topBarView:(PCTopBarView *)topBarView summaryButtonTapped:(UIButton *)button
 {
+    if (_hudView.summaryView.hidden) {
+        [_hudView showSummaryInView:self.view atPoint:button.center animated:YES];
+    } else {
+        [_hudView hideSummaryAnimated:YES];
+    }
 }
 
 - (void)topBarView:(PCTopBarView *)topBarView subscriptionsButtonTapped:(UIButton *)button
